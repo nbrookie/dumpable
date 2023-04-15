7aea16b671473717a913fae62e482683fa2017ef
nbrookie <nbrookie@gmail.com>
2023-04-14 21:29:24 -0700

    Support postgres, fix belongs_to association, some general formatting

--- lib/dumpable/dumper.rb
+++ lib/dumpable/dumper.rb
@@ -2,7 +2,7 @@ module Dumpable
   class Dumper
     attr_accessor :dumpee, :options, :id_padding, :dumps
 
-    def initialize(dumpee, options={})
+    def initialize(dumpee, options = {})
       @dumpee = dumpee
       @options = Dumpable.config.merge(options || {})
       @id_padding = @options[:id_padding] || (@dumpee.class.respond_to?(:dumpable_options) && @dumpee.class.dumpable_options[:id_padding]) || Dumpable.config.id_padding
@@ -32,6 +32,7 @@ module Dumpable
     end
 
     private
+
     def recursive_dump(object, dumps)
       if dumps.nil?
 
@@ -46,10 +47,10 @@ module Dumpable
         end
       elsif dumps.is_a?(Symbol) || dumps.is_a?(String)
         Array(object.send(dumps)).each do |child_object|
-          reflection = object.class.reflect_on_association(dumps.to_sym)
+          reflection = object.class.reflect_on_association(dumps.to_s)
           if reflection.macro == :belongs_to
-            object.send("#{reflection.association_foreign_key}=", object.id + @id_padding)
-          elsif [:has_many, :has_one].include? reflection.macro
+            object.send("#{reflection.foreign_key}=", child_object.id + @id_padding)
+          elsif [:has_many, :has_one].include?(reflection.macro) && reflection.options[:through].blank?
             if reflection.respond_to?(:foreign_key)
               child_object.send("#{reflection.foreign_key}=", object.id + @id_padding)
             else
@@ -64,14 +65,14 @@ module Dumpable
     # http://invisipunk.blogspot.com/2008/04/activerecord-raw-insertupdate.html
     def generate_insert_query(object)
       skip_columns = Array(@options[:skip_columns] || (object.class.respond_to?(:dumpable_options) && object.class.dumpable_options[:skip_columns])).map(&:to_s)
-      cloned_attributes = object.attributes.clone
+      cloned_attributes = object.attributes_before_type_cast.clone
       return nil unless cloned_attributes["id"].present?
       cloned_attributes["id"] += @id_padding
-      key_values = cloned_attributes.collect do |key,value|
+      key_values = cloned_attributes.collect do |key, value|
         [key, dump_value_string(value)] unless skip_columns.include?(key.to_s)
       end.compact
-      keys = key_values.collect{ |item| "`#{item[0]}`" }.join(", ")
-      values = key_values.collect{ |item| item[1].to_s }.join(", ")
+      keys = key_values.collect { |item| "\"#{item[0]}\"" }.join(", ")
+      values = key_values.collect { |item| item[1].to_s }.join(", ")
 
       "INSERT INTO #{object.class.table_name} (#{ keys }) VALUES (#{ values });"
     end
@@ -79,22 +80,26 @@ module Dumpable
     # http://invisipunk.blogspot.com/2008/04/activerecord-raw-insertupdate.html
     def dump_value_string(value)
       case value.class.to_s
-        when "Time", "ActiveSupport::TimeWithZone"
-          "'#{value.strftime("%Y-%m-%d %H:%M:%S")}'"
-        when "NilClass"
-          "NULL"
-        when "Fixnum"
-          value
-        when "String"
-          "'#{value.gsub(/'/, "\\\\'")}'"
-        when "FalseClass"
-          '0'
-        when "TrueClass"
-          '1'
-        when "ActiveSupport::HashWithIndifferentAccess"
-          "'#{value.to_yaml.gsub(/'/, "\\\\'")}'"
-        else
-          "'#{value}'"
+      when "Time", "ActiveSupport::TimeWithZone"
+        "\'#{value.strftime("%Y-%m-%d %H:%M:%S")}\'"
+      when "NilClass"
+        "NULL"
+      when "Fixnum" || "Integer"
+        value
+      when "String"
+        "E\'#{value.gsub(/'/, "\\\\'")}\'"
+      when "FalseClass"
+        "\'0\'"
+      when "TrueClass"
+        "\'1\'"
+      when "ActiveSupport::HashWithIndifferentAccess"
+        "\'#{value.to_yaml.gsub(/'/, "\\\\'")}\'"
+      when "Array"
+        "E\'#{value}\'"
+      when "Hash"
+        "\'#{value.to_json}\'"
+      else
+        "\'#{value}\'"
       end
     end
   end
